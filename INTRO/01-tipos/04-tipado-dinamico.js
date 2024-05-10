9012f5a30f71f00a9b71a0599f8dd5dac92faa8e
JonaRBL <121039246@upq.edu.mx>
2024-05-10 17:56:46 -0600

    imprimimos los tipos de datos de cada variable en la consola

--- INTRO/01-tipos/04-tipado-dinamico.js
+++ INTRO/01-tipos/04-tipado-dinamico.js
@@ -4,4 +4,8 @@ let verdadero = true;
 let undef;
 let nula = null;
 
+console.log(typeof numero);
 console.log(typeof nombre);
+console.log(typeof verdadero);
+console.log(typeof undef);
+console.log(typeof nula);
