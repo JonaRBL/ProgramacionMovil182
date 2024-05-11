efe1d01bab27109f68555605c686795bff50d4fe
JonaRBL <121039246@upq.edu.mx>
2024-05-10 19:05:13 -0600

    todos los operadores de comparación

---
+++ INTRO/02-operadores/03-comparacion.js
@@ -0,0 +1,12 @@
+let a = 10;
+
+console.log(a > 5);
+console.log(a >= 5);
+console.log(a < 5);
+console.log(a <= 5);
+
+console.log(a == 10);
+console.log(a != 10);
+console.log(a == '10');
+console.log(a === '10');//pregunta si los dos tipos de datos son iguales
+console.log(a !== '10');//pregunta si los dos tipos de datos son diferentes
