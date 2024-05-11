d92ad74abd3e9e487e0d5f1bf8250d29597ad222
JonaRBL <121039246@upq.edu.mx>
2024-05-10 18:35:51 -0600

    utilizamos varios parámetros y argumentos en la funcion

--- INTRO/01-tipos/09-argumentos.js
+++ INTRO/01-tipos/09-argumentos.js
@@ -1,6 +1,6 @@
-function suma(a) {
-    return a + 2;
+function suma(a, b) {//Parametro
+    return a + b;
 }
 
-let resultado = suma(5);
+let resultado = suma(5, 6);//Argumento
 console.log(resultado);
