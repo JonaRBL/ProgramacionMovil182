6d4735629be8b1ca2b6a3e31e3884d9321d87ab3
JonaRBL <121039246@upq.edu.mx>
2024-05-10 18:38:09 -0600

    Agregamos mas argumentos y los mostramos en la consola

--- INTRO/01-tipos/09-argumentos.js
+++ INTRO/01-tipos/09-argumentos.js
@@ -1,6 +1,8 @@
 function suma(a, b) {//Parametro
+    console.log(arguments)// como acceder a los valores de los parametros
     return a + b;
 }
 
-let resultado = suma(5, 6);//Argumento
+let resultado = suma(5, 6, 1, 2, 3);//Argumento
 console.log(resultado);
+console.log(typeof suma);
